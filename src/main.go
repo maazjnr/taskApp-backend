24cd53c9e55076c7fa215ecd615022499c32193a
maazjnr <goalmaaz@gmail.com>
2023-03-22 14:01:24 +0100

    fix error

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -43,13 +44,49 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("This is the HomePage")
 }
 
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	taskId := mux.Vars(r)
+	flag := false
+	for i := 0; i < len(tasks); i++ {
+		if taskId["id"] == tasks[i].ID {
+			json.NewEncoder(w).Encode(tasks[i])
+			break
+		}
+		if flag == false {
+			json.NewEncoder(w).Encode(map[string]string{"status": "error"})
+		}
+	}
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("This is the HomePage")
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("This is the HomePage")
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("This is the HomePage")
+}
+
 func handleRoutes() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/gettasks", getTasks).Methods("GET")
+	router.HandleFunc("/gettask{id}", getTask).Methods("GET")
+	router.HandleFunc("/create", createTask).Methods("POST")
+	router.HandleFunc("/delete/{id}", deleteTask).Methods("DELETE")
+	router.HandleFunc("/update/{id}", updateTask).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8082", router))
 }
 
 func main() {
-	allTasks()
 	handleRoutes()
 }
